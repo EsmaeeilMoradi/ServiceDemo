d8d21957f14aa1565a34245897ce4f32c58ac826
EsmaeeilMoradi <esmaeeilmoradi700@gmail.com>
2023-10-01 20:02:06 +0330

    Running a Service in background.

--- app/src/main/java/com/esm/servicedemo/MainActivity.java
+++ app/src/main/java/com/esm/servicedemo/MainActivity.java
@@ -2,13 +2,39 @@ package com.esm.servicedemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Intent serviceIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.e(getString(R.string.service_demo_tag), " MainActivity onCreate: Thread ID : " + Thread.currentThread().getId());
+
+
+        serviceIntent = new Intent(getApplicationContext(), MyService.class);
+
+
+        findViewById(R.id.buttonBindService).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService(serviceIntent);
+            }
+        });
+
+        findViewById(R.id.buttonUnBindService).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/esm/servicedemo/MyService.java
@@ -0,0 +1,28 @@
+package com.esm.servicedemo;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.e(getString(R.string.service_demo_tag), "  onDestroy: : Thread ID : " + Thread.currentThread().getId());
+
+        super.onDestroy();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.e(getString(R.string.service_demo_tag), "  onStartCommand: Thread ID : " + Thread.currentThread().getId());
+        stopSelf();
+        return super.onStartCommand(intent, flags, startId);
+    }
+}
